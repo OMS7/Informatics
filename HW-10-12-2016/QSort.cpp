bbf09ab76d77780cd42ddb10265d1ae3b764fb88
Opanasenko Mikhail <springeropa@mail.ru>
2016-12-09 21:24:29 +0300

    Update QSort.cpp

--- HW-10-12-2016/QSort.cpp
+++ HW-10-12-2016/QSort.cpp
@@ -1,22 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-template<class T> 
+template<class T>
 void QuickSort(vector<T> &v, int l, int r) {
-	if (l >= r) return;
 	T z = v[(l + r) / 2];
 	int ll = l, rr = r;
-	while (ll < rr) {
+	while (ll <= rr) {
 		while (v[ll] < z) ll++;
 		while (v[rr] > z) rr--;
-		swap(v[ll], v[rr]);
+		if (ll <= rr) swap(v[ll++], v[rr--]);
 	}
-	QuickSort(v, l, rr);
-	QuickSort(v, rr + 1, r);
+	if (l < rr) QuickSort(v, l, rr);
+	if (ll < r) QuickSort(v, ll, r);
 }
 int main() {
 	int n;
 	cin >> n;
-	vector<double> v(n);
+	vector<int> v(n);
 	for (int i = 0; i < n; i++) {
 		cin >> v[i];
 	}
