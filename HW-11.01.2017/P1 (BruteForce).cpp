bb8a8c2310efbfe24ce7f953f5cc4b574f1a3e5b
Opanasenko Mikhail <springeropa@mail.ru>
2016-12-31 19:35:54 +0300

    Update P1 (BruteForce).cpp

--- HW-11.01.2017/P1 (BruteForce).cpp
+++ HW-11.01.2017/P1 (BruteForce).cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 string num;
-int n;
-void f(int pos, int sum) {
+void print_palindrome(int pos, int sum, int n) {
 	if (pos >= (n + 1) / 2) {
 		if (sum % 9 == 0) cout << num << endl;
 		return;
@@ -10,15 +9,16 @@ void f(int pos, int sum) {
 	for (int i = 0; i <= 9; i++) {
 		if (i == 0 && pos == 0) continue;
 		num[pos] = num[n - pos - 1] = i + '0';
-		if (n % 2 && pos == n / 2) sum += i;
+		if (pos == n - pos - 1) sum += i;
 		else sum += 2 * i;
-		if (pos < (n + 1) / 2) f(pos + 1, sum);
-		if (n % 2 && pos == n / 2) sum -= i;
+		print_palindrome(pos + 1, sum, n);
+		if (pos == n - pos - 1) sum -= i;
 		else sum -= 2 * i;
 	}
 }
 int main() {
+	int n;
 	cin >> n;
 	num.resize(n);
-	f(0, 0);
+	print_palindrome(0, 0, n);
 }
