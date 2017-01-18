a5f2e3d38c8d13a5d08ee358ff96ca34d6f0d081
Opanasenko Mikhail <springeropa@mail.ru>
2017-01-18 16:34:08 +0300

    Update P8 (DP).cpp

--- HW-18.01.2017/P8 (DP).cpp
+++ HW-18.01.2017/P8 (DP).cpp
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef pair<int, int> pii;
 int migraine_minimization(int n) {
 	vector<int> dp(n + 1, 1e9);
 	dp[0] = 0;
 	for (int i = 1; i <= n; i++) {
-		int s = sqrt(i);
-		for (int q = 1; q <= s; q++) {
+		for (int q = 1; q * q <= i; q++) {
 			dp[i] = min(dp[i], dp[i - q*q] + 1);
 		}
 	}
-	return dp.back() * 4;
+	return dp[n] * 4;
 }
 int main() {
 	int n;
