994805b0ba9d0e581ffc8a85f90537a1fd7da93a
Opanasenko Mikhail <springeropa@mail.ru>
2017-05-01 09:57:52 +0300

    Create Encryptor.cpp

---
+++ RSA/Encryptor.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+int bpow(int a, int b, int m) {
+	int res = 1;
+	while (b) {
+		if (b % 2) res = (1ll * res * a) % m;
+		a = (1ll * a * a) % m;
+		b /= 2;
+	}
+	return res;
+}
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	int e, n;
+	cin >> e >> n;
+	string s;
+	cin >> s;
+	string ans;
+	for (int i = 0; i < s.size(); i++) {
+		int q = s[i];
+		q = bpow(q, e, n);
+		q %= 256;
+		ans.push_back(q);
+	}
+	cout << ans;
+}
