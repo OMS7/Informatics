fe1a67ef048514f9f422bc016643d1b6215a8b48
Opanasenko Mikhail <springeropa@mail.ru>
2017-10-25 11:20:09 +0300

    Create P.cpp

---
+++ T-25.10.2017/P.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> ans;
+
+bool bf(int s, vector<vector<int>> &g, vector<bool> &vis, vector<int> &ans) {
+	ans.push_back(s);
+	vis[s] = true;
+	for (int to : g[s]) {
+		if (ans.size() == g.size()) {
+			if (ans[0] == to) {
+				ans.push_back(to);
+				return true;
+			}
+			else continue;
+		}
+		if (vis[to]) continue;
+		if (bf(to, g, vis, ans)) return true;
+	}
+	vis[s] = false;
+	ans.pop_back();
+	return false;
+}
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	vector<vector<int>> g(n);
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+		g[a].push_back(b);
+		g[b].push_back(a);
+	}
+	vector<bool> vis(n);
+	vector<int> ans;
+	for (int i = 0; i < n; i++) {
+		if (bf(i, g, vis, ans)) {
+			cout << "YES\n";
+			for (int i = 0; i < n; i++) {
+				cout << ans[i] << " ";
+			}
+			return 0;
+		}
+	}
+	cout << "NO";
+}
